45324d6facc87eadf26d0454bcfbb1e62cdfcee3
CGJI58 <leemsk22@naver.com>
2022-09-30 14:10:17 +0900

    ~#6.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+
+  const onClickCounter = () => setValue((prev) => prev + 1);
+
+  console.log("I render everytime...");
+
+  useEffect(() => {
+    console.log("Call the API...");
+  }, []);
+
   return (
     <div>
+      <button onClick={onClickCounter}>{counter}</button>
       <h1 className={styles.title}>Welcome back!</h1>
       <Button text={"Continue"} />
     </div>
